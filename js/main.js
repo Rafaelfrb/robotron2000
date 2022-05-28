97f27aea662344afff0a344897eea462fc588629
Rafaelfrb <rafael.frb@hotmail.com>
2022-05-28 20:07:21 -0300

    Criando função que possibilita somar e subtrair braços

--- js/main.js
+++ js/main.js
@@ -1,7 +1,19 @@
-const robotron = document.querySelector('#robotron');
+/*const robotron = document.querySelector('#robotron');*/
+const subtrair = document.querySelector('#subtrair');
+const somar = document.querySelector('#somar');
+const braco = document.querySelector('#braco');
 
-robotron.addEventListener("click", dizOi);
 
-function dizOi() {
+
+/*robotron.addEventListener("click", dizOi);*/
+somar.addEventListener("click", (evento) =>  {
+    braco.value = parseInt(braco.value) + 1;
+});
+
+subtrair.addEventListener("click", (evento) =>  {
+    braco.value = parseInt(braco.value) - 1;
+});
+
+/*function dizOi() {
     console.log("oi");
-}
+}*/
